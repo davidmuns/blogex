4a620c61763b246a57f0e9fb6bf70349bfb397b8
David Gomez Muns <davidmuns@yahoo.es>
2022-09-29 20:42:33 +0200

    token-service works

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,6 +6,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { SignupComponent } from '../signup/signup.component';
 import { AuthService } from 'src/app/shared/services/auth.service';
 import { ToastrService } from 'ngx-toastr';
+import { TokenService } from 'src/app/shared/services/token.service';
 
 @Component({
   selector: 'app-login',
@@ -14,51 +15,47 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
-  hide:boolean = true;
+  hide: boolean = true;
 
   constructor(
     private readonly dialog: MatDialog,
     private readonly fb: FormBuilder,
-    public authSvc: AuthService,
+    private authSvc: AuthService,
+    private tokenService: TokenService,
     private readonly router: Router,
     private toastr: ToastrService
-    ) { }
+  ) { }
 
   ngOnInit(): void {
     this.initform();
   }
 
-  private initform():void{
+  private initform(): void {
     this.loginForm = this.fb.group({
       nombreUsuario: ['', Validators.required],
       password: ['', Validators.required]
     })
   }
 
-  signupOpen(){
+  signupOpen() {
     this.dialog.closeAll();
     this.dialog.open(SignupComponent);
   }
 
-  loginUser(login: Login){
-    console.log(login);
+  loginUser(login: Login) {
     this.authSvc.loginUser(login).subscribe({
       next: data => {
+        this.tokenService.setToken(data.token);
         this.toastr.success(`Welcome again ${login.nombreUsuario}!`, '', {
           timeOut: 3000, positionClass: 'toast-top-center'
         });
+        this.dialog.closeAll();
       },
-
       error: err => {
         this.toastr.error(err.error.message, '', {
           timeOut: 3000, positionClass: 'toast-top-center'
         });
       }
-
-
     });
-
-    
   }
-
 }
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { Login } from './../models/login';
 import { User } from './../models/user';
- import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
