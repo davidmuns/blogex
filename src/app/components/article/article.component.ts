8db6fc1bcf063ec8d32aa8f531efb222362efc1d
David Gomez Muns <davidmuns@yahoo.es>
2022-10-23 11:39:13 +0200

    upadated article

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -13,27 +13,36 @@ export class ArticleComponent implements OnInit {
 
   public post!: Article | undefined;
   imagenes!: Imagen[] | undefined;
-  
+  indice!: number;
 
-  constructor(private readonly route: ActivatedRoute, private articleSvc: ArticleService) {  }
+
+  constructor(
+    private readonly route: ActivatedRoute,
+    private articleSvc: ArticleService,
+  ) { }
 
   ngOnInit(): void {
     const idPost: number = this.route.snapshot.params['id'];   //snapshot.params['id'];
     this.getArticleById(idPost);
   }
 
-  private getArticleById(id: number){
+  private getArticleById(id: number) {
     this.articleSvc.getArticle(id)
-    .subscribe(data => this.post = data);
+      .subscribe(data => this.post = data);
   }
 
-  showGallery(id: number | undefined){
+  showGallery(id: number | undefined) {
     const idArticle: number = id as number;
     this.getImgsByArticleId(idArticle);
-    
+
+  }
+  getIndex(index: number) {
+    this.indice = index;
+    console.log('index: ', this.indice);
+
   }
 
-  private getImgsByArticleId(id: number){
+  private getImgsByArticleId(id: number) {
     this.articleSvc.getImagesByArticleId(id).subscribe({
       next: (data: Imagen[]) => {
         this.imagenes = data;
@@ -41,8 +50,8 @@ export class ArticleComponent implements OnInit {
     })
   }
 
-  hideGallery(){
+  hideGallery() {
     this.imagenes = [];
   }
- 
+
 }
