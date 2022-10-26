d57414de578a9b37a51623107f629f6a75a0eb8d
davidmuns <davidmuns@yahoo.es>
2022-10-26 12:21:00 +0200

    added model usuario

--- src/app/shared/models/article.ts
+++ src/app/shared/models/article.ts
@@ -1,16 +1,19 @@
+import { Usuario } from "./usuario";
+
 export interface Article {
-    id: number,
-    title: string,
-    img1?:  File,
-    alt1: string,
-    text1: string,
-    img2?:  File,
-    alt2?: string,
-    text2?: string,
-    img3?:  File,
-    alt3?: string,
-    text3?: string,
-    longitude: number,
-    latitude: number,
-    imagenPortada: string
+  id: number,
+  title: string,
+  img1?: File,
+  alt1: string,
+  text1: string,
+  img2?: File,
+  alt2?: string,
+  text2?: string,
+  img3?: File,
+  alt3?: string,
+  text3?: string,
+  longitude: number,
+  latitude: number,
+  imagenPortada: string,
+  usuario: Usuario
 }
