ed3517639e5874469682946b0827b5ba576783d3
David Gomez Muns <davidmuns@yahoo.es>
2022-11-08 09:32:39 +0100

    update url map

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -47,9 +47,9 @@ export class HomeComponent implements AfterViewInit {
         res.map(point => {
           this.postActual = point;
           marker([point.latitude, point.longitude]).addTo(map).bindPopup(`
-        <a href="http://localhost:4200/article/${point.id}">${point.title}</a>
+        <a href="https://blogex.netlify.app/article/${point.id}">${point.title}</a>
         <p class="text">${point.text1}</p>
-        <a href="http://localhost:4200/article/${point.id}"><img src="${point.imagenPortada}"></a>
+        <a href="https://blogex.netlify.app/article/${point.id}"><img src="${point.imagenPortada}"></a>
       `);
         });
         map.fitBounds([
@@ -71,9 +71,9 @@ export class HomeComponent implements AfterViewInit {
         res.map(point => {
           this.postActual = point;
           marker([point.latitude, point.longitude]).addTo(map2).bindPopup(`
-        <a href="http://localhost:4200/article/${point.id}">${point.title}</a>
+        <a href="https://blogex.netlify.app/article/${point.id}">${point.title}</a>
         <p class="text">${point.text1}</p>
-        <a href="http://localhost:4200/article/${point.id}"><img src="${point.imagenPortada}"></a>
+        <a href="https://blogex.netlify.app/article/${point.id}"><img src="${point.imagenPortada}"></a>
       `);
         });
         map2.fitBounds([
@@ -95,9 +95,9 @@ export class HomeComponent implements AfterViewInit {
         res.map(point => {
           this.postActual = point;
           marker([point.latitude, point.longitude]).addTo(map3).bindPopup(`
-        <a href="http://localhost:4200/article/${point.id}">${point.title}</a>
+        <a href="https://blogex.netlify.app/article/${point.id}">${point.title}</a>
         <p class="text">${point.text1}</p>
-        <a href="http://localhost:4200/article/${point.id}"><img src="${point.imagenPortada}" class="imgMap"></a>
+        <a href="https://blogex.netlify.app/article/${point.id}"><img src="${point.imagenPortada}" class="imgMap"></a>
       `);
         });
         map3.fitBounds([
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  users: "",
+  PRODUCT_URL: 'https://blogexapp.herokuapp.com/producto/',
+  AUTH_URL: 'https://blogexapp.herokuapp.com/auth/',
+  EMAIL_PASSWORD_URL: 'https://blogexapp.herokuapp.com/email-password/',
+  ARTICLE_BASE_URL: 'https://blogexapp.herokuapp.com/article/',
+  AUTHORIZATION: 'Authorization',
+  BEARER: 'Bearer ',
+  ARTICLES_LOCAL: './assets/articles.json'
 };
