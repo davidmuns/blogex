41eba77aa8b94d503cd9a4e452ce91623c502c00
David Gomez Muns <davidmuns@yahoo.es>
2022-12-14 13:58:40 +0100

    Update search.pipe.ts

--- src/app/shared/pipes/search.pipe.ts
+++ src/app/shared/pipes/search.pipe.ts
@@ -8,13 +8,8 @@ export class SearchPipe implements PipeTransform {
 
   transform(articles: Article[], title: string): Article[] {
     if (!title || title.length < 1) return articles;
-    let filteredarticles: Article[] = [];
-    for (const article of articles) {
-      if (article.title.toLowerCase().indexOf(title.toLowerCase()) > -1) {
-        filteredarticles.push(article)
-      }
-    }
-    return filteredarticles;
+    return articles
+      .filter(article => article.title.toLowerCase().indexOf(title.toLowerCase()) > -1);
   }
 
 }
