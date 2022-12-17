b7fb8ee1d419f42d1f6fdc4b5811590fa88598c9
David Gomez Muns <davidmuns@yahoo.es>
2022-12-17 21:16:42 +0100

    refactoring

--- src/app/components/article/article-gallery/articleGallery.component.ts
+++ src/app/components/article/article-gallery/articleGallery.component.ts
@@ -1,7 +1,7 @@
 import { Observable } from 'rxjs';
 import { Imagen } from './../../../shared/models/imagen';
 import { ArticleService } from './../../../shared/services/article.service';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-articleGallery',
@@ -11,7 +11,6 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ArticleGalleryComponent {
   @Input('articleId') articleId!: number | undefined;
   imagenes$!: Observable<Imagen[]>;
-  imagenes!: Imagen[];
   indice!: number;
 
   constructor(private articleSvc: ArticleService) { }
@@ -24,14 +23,10 @@ export class ArticleGalleryComponent {
     this.getImgsByArticleId(this.articleId);
   }
 
-  onChange(){
-    console.log('change');
-    
-  }
-
   private getImgsByArticleId(id: number | undefined) {
     this.imagenes$ = this.articleSvc.getImagesByArticleId(id);
   }
+
   hideGallery() {
     this.imagenes$ = new Observable();
     window.scroll(0, 0);
