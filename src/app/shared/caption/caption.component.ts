b0a58c06c9e0fc09ba8e2b976d7daea6227c6f84
David Gomez Muns <davidmuns@yahoo.es>
2023-02-08 18:37:19 +0100

    refactoring

--- src/app/shared/caption/caption.component.ts
+++ src/app/shared/caption/caption.component.ts
@@ -1,4 +1,4 @@
-import { ToastrService } from 'ngx-toastr';
+import { UtilsService } from './../services/utils.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 import { Component, Inject, OnInit } from '@angular/core';
@@ -15,43 +15,38 @@ export class CaptionComponent implements OnInit {
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: { imgId: string },
+    private utilsSvc: UtilsService,
     private readonly fb: FormBuilder,
     private readonly dialog: MatDialog,
-    private imageService: ImageService,
-    private toastr: ToastrService
-    ) { }
+    private imageService: ImageService
+    ) { };
 
   ngOnInit(): void {
     this.initform();
-  }
+  };
 
   private initform(): void {
     this.captionForm = this.fb.group({
       caption: ['', Validators.required]
-    })
-  }
+    });
+  };
 
   onSubmit(img: Imagen) { 
     if(this.captionForm.valid){
       img.id = this.data.imgId;
       this.addCaption(img);
-    }
-  }
+    };
+  };
 
   private addCaption(img: Imagen){
     this.imageService.addCaption(img).subscribe({
       next: (data: any) => {
-        this.toastr.success(data.mensaje, '', {
-          timeOut: 3000, positionClass: 'toast-top-center'
-        });
+        this.utilsSvc.showSnackBar(data.mensaje, 3000);
         this.dialog.closeAll();
       },
       error: (err: any) => {
-        this.toastr.error(err.error.message, '', {
-          timeOut: 3000, positionClass: 'toast-top-center'
-        });
+        this.utilsSvc.showSnackBar(err.error.message, 3000);
       }
-    })
-  }
-
-}
+    });
+  };
+};
