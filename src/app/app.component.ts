9b05a422124eea8be0209a8ae475662cd5419ea0
David Gomez Muns <davidmuns@yahoo.es>
2023-03-24 19:27:00 +0100

    scroll-up works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { filter } from 'rxjs';
 import { slider } from './route-animations';
@@ -7,20 +7,23 @@ declare let gtag: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
   animations: [
     slider
   ]
 })
 export class AppComponent {
   title = 'blogex';
+  show: boolean = false;
+  starShowing = 2500;
 
-  constructor(router: Router){
+  constructor(router: Router) {
     // https://youtu.be/Q2RTJxhBLkQ
     // Google analytics
     const navEndEvents$ = router.events
-    .pipe(
-      filter(event => event instanceof NavigationEnd)
-    );
+      .pipe(
+        filter(event => event instanceof NavigationEnd)
+      );
 
     navEndEvents$.subscribe((event: any) => {
       gtag('config', 'G-36QJW1606N', {
@@ -29,10 +32,31 @@ export class AppComponent {
     });
   }
 
-  onActivate(){
-    window.scroll(0, 0);
+  // https://www.youtube.com/watch?v=A3zhdTE4anc
+  @HostListener('window:scroll')
+  checkScroll() {
+    const scrollPosition =
+      window.scrollY ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop || 0;
+
+    if (scrollPosition >= this.starShowing) {
+      this.show = true;
+    } else {
+      this.show = false;
+    }
+
   }
 
+  onActivate() {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }
+
+  // animacion entre páginas
   prepareRoute(outlet: RouterOutlet) {
     return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
   }
--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -5,8 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
-  onActivate() {
-    window.scroll(0, 0);
-  }
-}
+export class FooterComponent {}
