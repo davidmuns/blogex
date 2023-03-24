d1c3d1c6629d446d64d3a19eb6b3183a5b73f381
David Gomez Muns <davidmuns@yahoo.es>
2023-03-24 12:49:16 +0100

    footer go up

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -5,4 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent { }
+export class FooterComponent {
+  onActivate() {
+    window.scroll(0, 0);
+  }
+}
