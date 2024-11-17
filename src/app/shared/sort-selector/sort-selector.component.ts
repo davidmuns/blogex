dbf1912b325938546b2d02b46f76787eabc9ea5e
David Gomez Muns <davidmuns@yahoo.es>
2024-11-17 19:15:28 +0100

    translate works

--- src/app/shared/sort-selector/sort-selector.component.ts
+++ src/app/shared/sort-selector/sort-selector.component.ts
@@ -1,29 +1,43 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 import { Article } from '../models/article';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sort-selector',
   templateUrl: './sort-selector.component.html',
   styleUrls: ['./sort-selector.component.scss']
 })
-export class SortSelectorComponent implements OnInit {
-
+export class SortSelectorComponent implements OnInit, OnDestroy {
   @Output() selectedOption = new EventEmitter<string>();
   option = '';
-  sortOptions = [
-    { value: 'title-az', text: this.translateSvc.instant('user-blog.title-az') },
-    { value: 'title-za', text: this.translateSvc.instant('user-blog.title-za') },
-    { value: 'older', text: this.translateSvc.instant('user-blog.older') },
-    { value: 'newer', text: this.translateSvc.instant('user-blog.newer') },
-  ];
+  sortOptions: { value: string; text: string }[] = [];
+  langChangeSubscription!: Subscription;
 
-  constructor( private readonly translateSvc: TranslateService) { }
+  constructor(private readonly translateSvc: TranslateService) { }
 
-  ngOnInit(): void {}
-
-  onSortBy(){
-    this.selectedOption.emit(this.option);
+  ngOnInit(): void {
+    this.setSortOptions(); // Inicializar las opciones al cargar
+    this.langChangeSubscription = this.translateSvc.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.setSortOptions(); // Actualizar opciones cuando cambie el idioma
+    });
   }
 
+  ngOnDestroy(): void {
+    // Liberar la suscripción para evitar fugas de memoria
+    this.langChangeSubscription.unsubscribe();
+  }
+
+  setSortOptions(): void {
+    this.sortOptions = [
+      { value: 'title-az', text: this.translateSvc.instant('user-blog.title-az') },
+      { value: 'title-za', text: this.translateSvc.instant('user-blog.title-za') },
+      { value: 'older', text: this.translateSvc.instant('user-blog.older') },
+      { value: 'newer', text: this.translateSvc.instant('user-blog.newer') }
+    ];
+  }
+
+  onSortBy(): void {
+    this.selectedOption.emit(this.option);
+  }
 }
