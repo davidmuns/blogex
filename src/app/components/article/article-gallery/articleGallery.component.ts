68a63b7aeb2ec6cac9710d8e44ea379e63153222
David Gomez Muns <davidmuns@yahoo.es>
2024-12-16 14:01:33 +0100

    transicion suave entre imagenes del carrusel

--- src/app/components/article/article-gallery/articleGallery.component.ts
+++ src/app/components/article/article-gallery/articleGallery.component.ts
@@ -1,10 +1,9 @@
 import { UtilsService } from './../../../shared/services/utils.service';
 import { Video } from './../../../shared/models/video';
 import { VideoService } from './../../../shared/services/video.service';
-import { Observable } from 'rxjs';
 import { Imagen } from './../../../shared/models/imagen';
 import { ArticleService } from './../../../shared/services/article.service';
-import { Component, HostListener, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import AOS from 'aos';
 import { DeleteComponent } from '../../crud/delete/delete.component';
 import { CaptionComponent } from 'src/app/shared/caption/caption.component';
@@ -28,7 +27,7 @@ export class ArticleGalleryComponent implements OnInit {
   playerVars = {
     cc_lang_pref: 'es'
   }
-  isZoomed!: number | null;
+
   constructor(
     public tokenSvc: TokenService,
     private readonly articleSvc: ArticleService,
@@ -68,46 +67,30 @@ export class ArticleGalleryComponent implements OnInit {
 
   openModal(index: number) {
     this.indice = index;
-    // Filtrar solo las imágenes
     const imageFiles = this.imagenes.filter(file => file.fileType === 'image');
-
-    // Crear la lista de URLs de las imágenes
     this.urlmages = imageFiles.map(file => file.url);
-
-    // Obtener la URL del archivo seleccionado
     const clickedFile = this.imagenes[index];
-
-    // Verificar si el archivo es una imagen y encontrar su índice en la lista filtrada
     if (clickedFile.fileType === 'image') {
       this.indice = imageFiles.findIndex(file => file.id === clickedFile.id); // Encuentra el índice dentro de las imágenes
-      this.isModalOpen = true; // Abre el modal
+      this.isModalOpen = true;
     }
   }
 
-  // Cierra el modal
   closeCarousel(): void {
     this.isModalOpen = false;
   }
 
-  // Navega a la imagen anterior
   prevImage(): void {
     this.indice = (this.indice - 1 + this.urlmages.length) % this.urlmages.length;
   }
 
-  // Navega a la siguiente imagen
   nextImage(): void {
     this.indice = (this.indice + 1) % this.urlmages.length;
   }
 
-  // Alterna el zoom al hacer clic en una imagen
-  toggleZoom(event: Event, index: number): void {
-    event.stopPropagation(); // Evita que el evento se propague al documento
-    this.isZoomed = this.isZoomed === index ? null : index;
+  getTransform(): string {
+    return `translateX(-${this.indice * 100}%)`;
   }
 
-  // Detecta clics fuera de las imágenes
-  @HostListener('document:click', ['$event'])
-  handleClickOutside(): void {
-    this.isZoomed = null; // Resetea el zoom
-  }
+ 
 }
