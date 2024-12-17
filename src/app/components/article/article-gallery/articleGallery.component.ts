3393988a0cdb30604f0dacb13f9d3cb368d8d2f2
David Gomez Muns <davidmuns@yahoo.es>
2024-12-17 14:20:55 +0100

    imgs transition on carrousel for mobile devices

--- src/app/components/article/article-gallery/articleGallery.component.ts
+++ src/app/components/article/article-gallery/articleGallery.component.ts
@@ -1,8 +1,6 @@
 import { UtilsService } from './../../../shared/services/utils.service';
 import { Video } from './../../../shared/models/video';
-import { VideoService } from './../../../shared/services/video.service';
 import { Imagen } from './../../../shared/models/imagen';
-import { ArticleService } from './../../../shared/services/article.service';
 import { Component, Input, OnInit } from '@angular/core';
 import AOS from 'aos';
 import { DeleteComponent } from '../../crud/delete/delete.component';
@@ -24,16 +22,18 @@ export class ArticleGalleryComponent implements OnInit {
   urlmages: string[] = [];
   isModalOpen: boolean = false;
   indice: number = 0;
+  isDragging = false;
+  startX = 0;
+  currentTranslate = 0;
+  prevTranslate = 0;
   playerVars = {
     cc_lang_pref: 'es'
   }
 
   constructor(
     public tokenSvc: TokenService,
-    private readonly articleSvc: ArticleService,
     private readonly dialog: MatDialog,
-    private readonly utilsSvc: UtilsService,
-    private readonly videoSvc: VideoService) {}
+    private readonly utilsSvc: UtilsService) {}
 
   ngOnInit(): void {
     AOS.init();
@@ -81,15 +81,86 @@ export class ArticleGalleryComponent implements OnInit {
   }
 
   prevImage(): void {
-    this.indice = (this.indice - 1 + this.urlmages.length) % this.urlmages.length;
+    // this.indice = (this.indice - 1 + this.urlmages.length) % this.urlmages.length;
+    this.indice--;
+    this.syncTranslateWithIndex(true, 'arrow');
+
   }
 
   nextImage(): void {
-    this.indice = (this.indice + 1) % this.urlmages.length;
+    // this.indice = (this.indice + 1) % this.urlmages.length;
+    this.indice++;
+    this.syncTranslateWithIndex(true, 'arrow');
   }
 
   getTransform(): string {
     return `translateX(-${this.indice * 100}%)`;
+  }
+
+  onTouchStart(event: TouchEvent): void {
+    this.isDragging = true;
+    this.startX = event.touches[0].clientX;
+    
+    // Pausar transición mientras se arrastra
+    const track = document.querySelector('.carousel-track') as HTMLElement;
+    track.style.transition = 'none';
+    
+  }
+   // Detectar movimiento durante el arrastre
+   onTouchMove(event: TouchEvent): void {
+    if (!this.isDragging) return;
+    const currentX = event.touches[0].clientX;
+    const deltaX = currentX - this.startX;
+    this.currentTranslate = this.prevTranslate + deltaX;
+    const track = document.querySelector('.carousel-track') as HTMLElement;
+    track.style.transform = `translateX(${this.currentTranslate}px)`;
+  }
+
+  
+  onTouchEnd(): void {
+    if (!this.isDragging) return;
+
+    this.isDragging = false;
+
+    const itemWidth = window.innerWidth;
+    const movedBy = this.currentTranslate - this.prevTranslate;
+
+    // Determinar si el movimiento es suficiente para cambiar de imagen
+    if (movedBy < -itemWidth * 0.10 && this.indice < this.urlmages.length - 1) {
+      this.indice++;
+    } else if (movedBy > itemWidth * 0.10 && this.indice > 0) {
+      this.indice--;
+    }
+
+    this.syncTranslateWithIndex(true, 'touch');
+  }
+
+  private syncTranslateWithIndex(applyTransition = false, action: 'touch' | 'arrow' = 'touch'): void {
+    const itemWidth = window.innerWidth;
+    this.currentTranslate = -this.indice * itemWidth;
+    this.prevTranslate = this.currentTranslate;
+
+    const track = document.querySelector('.carousel-track') as HTMLElement;
+
+    if (applyTransition) {
+      track.style.transition =
+        action === 'touch'
+          ? 'transform 1s ease' // Transición móvil
+          : 'transform 1s cubic-bezier(0.68, -0.55, 0.27, 1.55)'; // Transición flechas
+    }
+
+    track.style.transform = `translateX(${this.currentTranslate}px)`;
+
+    // if (applyTransition) {
+    //   setTimeout(() => {
+    //     track.style.transition = 'none'; // Desactivar transición para futuros movimientos
+    //   }, action === 'touch' ? 1000 : 1000); // Duración según el tipo de interacción
+    // }
+    if (applyTransition) {
+      setTimeout(() => {
+        track.style.transition = 'none'; // Desactivar transición para futuros movimientos
+      }, 1000); // Duración según el tipo de interacción
+    }
   }
 
  
